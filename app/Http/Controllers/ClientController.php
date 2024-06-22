0f415ab02562b19baa55655323983b4cbefbaa9a
cristhiankelm <criskp007@gmail.com>
2024-06-22 12:27:08 -0400

    add error handling crud clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
 use App\Models\Client;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 
 class ClientController extends Controller
 {
     // Método para exibir todos os clientes
-    public function index()
+    public function index(): JsonResponse
     {
         // Buscar todos os clientes
         $clients = Client::query()->get();
@@ -19,7 +21,7 @@ class ClientController extends Controller
     }
 
     // Método para criar um novo cliente usando form request
-    public function store(StoreClientRequest $request)
+    public function store(StoreClientRequest $request): JsonResponse
     {
         // Receber os dados do Form Request
         $data = $request->validated();
@@ -32,39 +34,55 @@ class ClientController extends Controller
     }
 
     // Método para exibir os detalhes de um cliente específico
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
-        // Buscar o cliente com o ID informado
-        $client = Client::query()->findOrFail($id);
+        try {
+            // Buscar o cliente com o ID informado
+            $client = Client::findOrFail($id);
 
-        // Retornar os detalhes do cliente em formato JSON
-        return response()->json($client);
+            // Retornar os detalhes do cliente em formato JSON
+            return response()->json($client);
+        } catch (ModelNotFoundException $e) {
+            // Retornar uma mensagem de erro se o cliente não for encontrado
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
+        }
     }
 
     // Método para atualizar um cliente existente
-    public function update(UpdateClientRequest $request, string $id)
+    public function update(UpdateClientRequest $request, string $id): JsonResponse
     {
-        // Receber os dados do Form Request
-        $data = $request->validated();
+        try {
+            // Receber os dados do Form Request
+            $data = $request->validated();
 
-        // Buscar o cliente com o ID informado
-        $client = Client::query()->findOrFail($id);
-        // Atualizar os dados do cliente com os dados validados do Form Request
-        $client->update($data);
+            // Buscar o cliente com o ID informado
+            $client = Client::query()->findOrFail($id);
+            // Atualizar os dados do cliente com os dados validados do Form Request
+            $client->update($data);
 
-        // Retornar os detalhes do cliente atualizado em formato JSON
-        return response()->json($client);
+            // Retornar os detalhes do cliente atualizado em formato JSON
+            return response()->json($client);
+        } catch (ModelNotFoundException $e) {
+            // Retornar uma mensagem de erro se o cliente não for encontrado
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
+        }
     }
 
     // Método para excluir um cliente existente
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
-        // Buscar o cliente com o ID informado
-        $client = Client::query()->findOrFail($id);
-        // Deletar o cliente com o ID informado
-        $client->delete();
+        try {
+            // Buscar o cliente com o ID informado
+            $client = Client::findOrFail($id);
 
-        // Retornar uma mensagem de sucesso em formato JSON
-        return response()->json(['message' => 'Cliente deletado com sucesso'], 200);
+            // Deletar o cliente com o ID informado
+            $client->delete();
+
+            // Retornar uma mensagem de sucesso em formato JSON
+            return response()->json(['message' => 'Cliente deletado com sucesso'], 200);
+        } catch (ModelNotFoundException $e) {
+            // Retornar uma mensagem de erro se o cliente não for encontrado
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
+        }
     }
 }
