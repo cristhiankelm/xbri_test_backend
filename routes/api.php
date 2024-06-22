4c087c7297ca2b06844625f58af296ebef0df7de
cristhiankelm <criskp007@gmail.com>
2024-06-22 12:31:31 -0400

    add typing to relationships

--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,17 @@ Route::name('api.')->group(function () {
         Route::post('/login/do', 'attempt')->name('login.do')->withoutMiddleware('auth-api');
         Route::get('/logout', 'logout')->name('logout')->middleware('auth:sanctum');
     });
-});
 
-Route::controller(ClientController::class)
-->group(function () {
-    Route::get('/clientes', 'index')->name('clients.index');
-    Route::get('/cliente/{id}', 'show')->name('clients.show');
-    Route::post('/cliente', 'store')->name('clients.store');
-    Route::put('/cliente/{id}', 'update')->name('clients.update');
-    Route::delete('/cliente/{id}', 'destroy')->name('clients.destroy');
+
+    // Rotas para crud clientes
+    Route::controller(ClientController::class)
+    ->group(function () {
+        Route::get('/clientes', 'index')->name('clients.index');
+        Route::get('/cliente/{id}', 'show')->name('clients.show');
+        Route::post('/cliente', 'store')->name('clients.store');
+        Route::put('/cliente/{id}', 'update')->name('clients.update');
+        Route::delete('/cliente/{id}', 'destroy')->name('clients.destroy');
+    });
 });
 
 // Rota default laravel API, teste de middleware de autenticação
